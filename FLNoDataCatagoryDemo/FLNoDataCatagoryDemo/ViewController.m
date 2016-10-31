be7b5db2fb1a4e6d334f92d7cefd8fb55dc6e2f6
clarence <13751855378@163.com>
2016-10-31 10:46:43 +0800

    测试通过

--- FLNoDataCatagoryDemo/FLNoDataCatagoryDemo/ViewController.m
+++ FLNoDataCatagoryDemo/FLNoDataCatagoryDemo/ViewController.m
@@ -20,6 +20,11 @@
     [self.view fl_showNoDataViewOperation:^{
         NSLog(@"hello world");
     }];
+    
+    __weak typeof(self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [weakSelf.view fl_hideNoDataView];
+    });
 }
 
 
